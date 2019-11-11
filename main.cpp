4bef94ce39681bd2270f0e32c112b2a400e18924
IllCher <50427226+IllCher@users.noreply.github.com>
2019-11-11 19:21:33 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <thread>
+#include <string>
+using namespace std;
+#define LENGTH 4
+#define CORES 2
+
+int char_to_int(char c) {
+    if (c >= '0' && c <= '9') {
+        return (c-'0');
+    } else {
+        return (c-'W');
+    }
+}
+
+bool a_lower_or_eq_b(string a, string b) {
+    int v1 = 0;
+    int v2 = 0;
+    int i = 0;
+    int k = 1;
+    while (a[i] != '\0') {
+        v1 += char_to_int(a[i])*k;
+        k*=10;
+        i++;
+    }
+    i = 0;
+    k = 1;
+    while (b[i] != '\0') {
+        v2 += char_to_int(b[i])*k;
+        k*=10;
+        i++;
+    }
+    if (v1 <= v2) {
+        return  true;
+    }
+    return false;
+}
+
+void merge(string *orig, int left, int mid, int right, string *modif) {
+    int l = left;
+    int r = mid;
+    for (int i = left; i < right; i++)
+        if (l < mid && (r >= right || a_lower_or_eq_b(orig[l], orig[r])))
+            modif[i] = orig[l++];
+        else
+            modif[i] = orig[r++];
+    for (int i = left; i < right; i++)
+        orig[i] = modif[i];
+}
+void split(string *orig, int left, int right, string *modif) {
+    if (right - left < 2)return;
+    split(orig, left, (left + right) / 2,modif);
+    split(orig, (left + right) / 2, right,modif);
+    merge(orig, left, (left + right) / 2, right,modif);
+}
+void merge_sort(string *in, int length) {
+    string temp[LENGTH];
+    thread threads[CORES];
+    for (int i = 0; i < CORES; i++)
+        threads[i] = thread(&split,in,i*LENGTH/CORES,(i+1)*LENGTH/CORES,temp);
+    for (int i = 0; i < CORES; i++)
+        threads[i].join();
+    for (int i = CORES/2; i > 0; i = i >> 1)
+        for (int j = 0; j < i; j++){
+            int left = (j)*LENGTH / i;
+            int right = (j + 1)*LENGTH / i;
+            merge(in, left, (left + right) / 2, right, temp);
+        }
+}
+int main(int argc, char *argv[]) {
+
+    string array[LENGTH];
+    for (int i = 0; i < LENGTH; i++)
+        cin >> array[i];
+    merge_sort(array, LENGTH);
+    for (int i = 0; i < LENGTH; i++)
+        cout << array[i] << endl;
+    return 0;
+}
